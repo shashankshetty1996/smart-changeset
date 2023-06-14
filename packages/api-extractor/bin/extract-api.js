5cf63292b300e34436d33acc41e2a1f380c3b677
Shashank Shetty <sshetty3@atlassian.com>
2023-06-14 17:03:41 +0530

    INNO-007 | API extractor base setup

---
+++ packages/api-extractor/bin/extract-api.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+/* eslint-disable */
+/* prettier-ignore */
+
+const fs = require('fs');
+const path = require("path");
+const project = path.join(__dirname, "../tsconfig.json");
+const dev = fs.existsSync(project);
+
+let cliPath = "dist/cli/api-extractor.cli.js";
+if (dev) {
+  require("ts-node").register({ project });
+  cliPath = "cli/api-extractor.cli.ts";
+}
+
+require(path.join("..", cliPath))
+  .run()
+  .catch((error) => {
+    if (typeof error === "number") {
+      process.exit(error);
+    }
+    console.error(error);
+    process.exit(1);
+  });
---
+++ packages/api-extractor/cli/api-extractor.cli.ts
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+export async function run() {
+  console.log(chalk.green("Hello world! 🌈"));
+}
